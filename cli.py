b6d8ffaf8e83b6d7a6d09811c9f24cc880f9e920
Thomas Guillet <thomas@palya.fr>
2025-06-24 14:03:53 +0200

    Refacto CLI

--- cli.py
+++ cli.py
@@ -1,9 +1,21 @@
-import access
+from access import update as update_fct
+import click
 
 
-def main():
-    access.update()
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def access():
+    pass
+
+
+@access.command()
+def update():
+    update_fct()
 
 
 if __name__ == "__main__":
-    main()
+    cli()
