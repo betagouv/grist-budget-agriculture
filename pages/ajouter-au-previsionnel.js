1f0445c94972cb1911b20ac5b905756220b0ca02
Thomas Guillet <thomas@palya.fr>
2025-06-30 12:40:30 +0200

    Ajoute une page pour ajouter une ligne au prévisionnel (lorsqu'un contrat n'apparait pas)

---
+++ pages/ajouter-au-previsionnel.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import jsonwebtoken from "jsonwebtoken";
+
+import { filterMonthRowRecords } from "../lib/month.js";
+
+export default function InspectOnRecords() {
+  const searchParams = useSearchParams();
+  const year = searchParams.get("year");
+  const [record, setRecord] = useState();
+  const [allMonths, setAllMonths] = useState();
+  const [month, setMonth] = useState();
+
+  useEffect(() => {
+    window.grist.ready({
+      requiredAccess: "full",
+    });
+    window.grist.onRecord(async (record) => {
+    	setRecord(record);
+    });
+
+    async function getMonths() {
+      const recordData = await window.grist.docApi.fetchTable("Mois_de_facturation")
+      setAllMonths(recordData);
+    }
+    getMonths()
+  }, []);
+
+  useEffect(() => {
+    if (!year || !allMonths) {
+      return
+    }
+    const months = filterMonthRowRecords(allMonths, parseInt(year))
+    setMonth(months[0])
+  }, [year, allMonths])
+
+  function addRow() {
+    window.grist.getTable("Conso_mensuelle").create([{fields: {
+      Contrat_Freelance: record.id,
+      Nb_jours_factures: 0,
+      Mois: month.id
+    }}])
+  }
+  function show(month) {
+    if (!month) {
+      return
+    }
+    const d = new Date(month.c1er_du_mois*1000)
+    return d.toLocaleDateString(undefined, { month: "long", year: "numeric"})
+  }
+
+  return (
+    <div className="fullPage">
+      { record ? (
+        record.Conso >= 1 ? <div><i>Le contrat freelance apparait déjà dans le prévisionnel.</i></div> :
+            <button onClick={addRow}>Ajouter une ligne au prévisionnel pour ce contrat freelance<br/>
+              <i>En ajoutant une conso mensuelle prévisionnelle<br/>avec zéro jour sur le mois de {show(month)}</i></button>
+        ) : <div>Un contrat freelance doit être sélectionné.</div>
+      }
+    </div>
+  );
+}
